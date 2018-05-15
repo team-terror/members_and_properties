f82a64e269af53c8e5ffa143ea24fa1aa7c68aa7
kellenbarber@gmail.com <bw5g@M00972301.nordstrom.net>
2018-05-15 11:30:00 -0700

    login wip

--- login/member-login.js
+++ login/member-login.js
@@ -1,4 +1,33 @@
 var AWS = require('aws-sdk'),
-    uuidv1 = require('uuid/v1'),
     bcrypt = require('bcrypt-nodejs'),
-	documentClient = new AWS.DynamoDB.DocumentClient();
+    documentClient = new AWS.DynamoDB.DocumentClient();
+
+var lambda = new AWS.Lambda({
+    region: 'us-west-2' //change to your region
+    });
+
+
+exports.login = function(event, context, callback) {
+    lambda.invoke({
+        FunctionName: "terrorGetMember",
+        Payload: JSON.stringify({
+            email: event.email
+        }, null, 2)
+    }, function(err, data) {
+        if (err) {
+            console.error(err);
+        }
+        if (data.Payload) {
+            var hashed_password = data.Payload.Item.hashed_password;
+            console.info(data.Payload);
+            bcrypt.compare(event.password, hashed_password, function(err, data) {
+                console.info(event.password);
+                console.info(hashed_password);
+                if (err) {
+                    console.error(err);
+                }
+                console.info(data);
+            });
+        }
+    });
+}
